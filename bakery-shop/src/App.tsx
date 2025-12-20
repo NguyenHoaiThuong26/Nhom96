826cd7d0207560ba5d6bbc9e4f25d0d3bb9d2daf
NguyenHoaiThuong26 <21130559@st.hcmuaf.edu.vn>
2025-12-20 13:12:26 +0700

    create frontend project

---
+++ bakery-shop/src/App.tsx
@@ -0,0 +1,13 @@
+import './App.css'
+
+function App() {
+  return (
+    <div className="min-h-screen bg-black flex items-center justify-center">
+      <h1 className="text-6xl text-green-400 font-bold">
+        TAILWIND OK ✅
+      </h1>
+    </div>
+  )
+}
+
+export default App
